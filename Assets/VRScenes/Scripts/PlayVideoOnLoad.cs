1a4b8b5873a2db1731f593a95907b818a71b949e
Joel Austin <Jta26@pitt.edu>
2017-11-13 16:53:07 -0500

    2/3 Assets Folder

---
+++ Assets/VRScenes/Scripts/PlayVideoOnLoad.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayVideoOnLoad : MonoBehaviour {
+	#if !UNITY_ANDROID
+    public MovieTexture _movie;
+	#endif
+
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_ANDROID
+		string moviePath = this.GetComponent<MeshRenderer>().materials[0].name;
+
+		Handheld.PlayFullScreenMovie(moviePath);
+
+		#else
+		_movie.loop = true;
+		_movie.Play();
+
+		#endif
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
